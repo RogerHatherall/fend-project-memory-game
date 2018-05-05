4d5dd3df141fc3fe0c9851fa8549dea48916f001
Roger Hatherall <roger.hatherall@gmail.com>
2018-05-05 17:41:26 +0100

    Create function to display a card.

--- js/app.js
+++ js/app.js
@@ -62,3 +62,12 @@ deck.appendChild(fragment);
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ /* Create function to display a card. */
+
+function displayCard(clickedCard) {
+  clickedCard.className="card open show disabled";
+  return(clickedCard);
+}
+
+displayCard(clickedCard);
